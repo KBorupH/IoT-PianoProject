c22afcd459fde9e69df9a8b080c63b2dd0f68488
MP-Hartvig <mip@hartvigconsult.dk>
2024-06-21 10:16:04 +0200

    .

---
+++ IoTPiano/PianoUI/Views/ManualPlayControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PianoUI.Views
+{
+    public partial class ManualPlayControl : UserControl
+    {
+        public ManualPlayControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
